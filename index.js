af03d2b56eddf6de108e0a1e5819a9116f52b659
gabrieldeoa <gabrielaraujo.dev@gmail.com>
2020-05-28 19:54:16 -0300

    first message send

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+import Discord from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const webhookClient = new Discord.WebhookClient(
+    process.env.DWB_ID,
+    process.env.DWB_TOKEN
+);
+
+webhookClient.send(`Hello World`);
